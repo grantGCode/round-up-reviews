4b0195624561e9a2fb4cc20635e1d978a1411e3d
Grant Bentley <gbentley0618@comcast.net>
2024-09-23 10:42:29 -0500

    add producct modal

--- components/ProductModal.tsx
+++ components/ProductModal.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import Image from 'next/image';
+import VectorX from '../public/Vector-X.png';
+import JacketOne from '../public/JacketOne.jpg';
+import StarYellow from '../public/StarYellow.png';
+import StarGray from '../public/StarGray.png';
+import Button from './button';
 
 function ProductModal() {
   return (
-    <div>ProductModal</div>
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+      <div className="p-8 border w-96 shadow-lg bg-[#E5E5E5]">
+          <Image src={VectorX} alt='X'></Image>
+          <div className='flex flex-col justify-center items-center'>
+            <Image className='shadow' src={JacketOne} alt='Jacket One' />
+            <h4 className='mt-10'>ROSA Jacket</h4>
+            <h5>Rating</h5>
+            <p>
+              Let us know what your experience
+              with your product has been like 
+              for you.
+            </p>
+            <div id='Stars'className='flex flex-row justify-center items-center'>
+              <Image src={StarYellow} alt={'Yellow Star'}></Image>
+              <Image src={StarYellow} alt={'Yellow Star'}></Image>
+              <Image src={StarYellow} alt={'Yellow Star'}></Image>
+              <Image src={StarGray} alt={'Gray Star'}></Image>
+              <Image src={StarGray} alt={'Gray Star'}></Image>
+            </div>
+            <input
+              className='h-'
+              type="text" 
+              placeholder="Leave a comment (optional)"
+              required
+              autoCapitalize="off"
+              autoCorrect="off" 
+            />
+            <Button>Submit</Button>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface ButtonProps {
   onClick?: () => void;
   children: React.ReactNode;
@@ -7,12 +5,11 @@ interface ButtonProps {
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
 }
-
 const Button: React.FC<ButtonProps> = ({ onClick, children, className = '', type = 'button', disabled = false }) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:bg-gray-400 disabled:cursor-not-allowed transition ${className}`}
+      className={`px-4 py-2 bg-[#005FF6] text-white hover:bg-blue-700 disabled:bg-gray-400 disabled:cursor-not-allowed transition ${className}`}
       type={type}
       disabled={disabled}
     >
@@ -20,5 +17,4 @@ const Button: React.FC<ButtonProps> = ({ onClick, children, className = '', type
     </button>
   );
 };
-
 export default Button;
