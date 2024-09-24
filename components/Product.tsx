1b014c06ed0d3724d9d02ce9a98a0dd5a833e0ae
Grant Bentley <gbentley0618@comcast.net>
2024-09-24 08:33:00 -0500

    add missing files from last commit

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 import JacketOne from '../public/JacketOne.jpg'
 import JacketTwo from '../public/JacketTwo.jpg'
-import Button from '../components/button'
 
-export default function Product() {
-    return (
+// import ProductModal from '../components/ProductModal';
+
+/*type SearchParamProps = {
+  searchParams: Record<string, string> | null | undefined;
+};*/
+
+export default function Product(/*{ searchParams }: SearchParamProps*/) {
+  /*const show = searchParams?.show;*/
+
+  return (
     <div>
         <ul>
             <li className='flex flex-col justify-center items-center'>
                 <Image className='shadow' src={JacketOne} alt='Jacket One' />
                 <h4 className='mt-10'>ROSA Jacket</h4>
-                <Button className='mt-5'>Rate</Button>
+                <Link className='mt-5' href='/?show=true'>Rate</Link>
             </li>
-            <li className='flex flex-col justify-center items-center mt-10'>
-                <Image className='shadow' src={JacketTwo} alt='Jacket Two' />
+            <li className='flex flex-col justify-center items-center'>
+                <Image className='shadow'src={JacketTwo} alt='Jacket Two' />
                 <h4 className='mt-10'>ROSA Jacket</h4>
-                <Button className='mt-5'>Rate</Button>
+                <Link className='mt-5' href='/?show=true'>Rate</Link>
             </li>
         </ul>
+        {/* {show && <ProductModal />} */}
     </div>
   )
 }
