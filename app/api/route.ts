8a8efcd6b3329aa72d2245a4e01ded979d3111c0
Grant Bentley <gbentley0618@comcast.net>
2024-11-06 15:50:43 -0600

    format get request

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,16 +2,21 @@ import { NextResponse } from "next/server";
 import db from "../../db/db";
 
 export async function GET() {
-        const result = await new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM reviews"
-            db.query(sql, (err: unknown, result: []) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
+    const result = await new Promise((resolve, reject) => {
+        const sql = `SELECT 
+            reviews.id, product.product_name, reviews.star_rating, vender.vender_name, reviews.written_comment
+            FROM reviews 
+            INNER JOIN product
+            ON reviews.product_id = product.id
+            INNER JOIN vender
+            ON product.vender_id = vender.id;`
+        db.query(sql, (err: unknown, result: []) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
             }
-            );
         });
+    });
     return NextResponse.json(result)
-}
+};
