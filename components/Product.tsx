fe782b0b226aa2e65397408849a4b4937939cd27
Grant Bentley <gbentley0618@comcast.net>
2024-12-11 15:28:43 -0600

    add revalidater for Products commponent every 30 sec

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,27 +1,37 @@
 import Link from 'next/link'
 
 
-type Review = {
+// type Review = {
+//   id: number;
+//   product_name: string; 
+//   vender_name: string;
+//   star_rating: number;
+//   written_comment: string;
+// };
+
+type productInfo = {
   id: number;
-  product_name: string; 
+  product_name: string;
   vender_name: string;
-  star_rating: number;
-  written_comment: string;
 };
 
 
 export default async function Product(){
-  const res = await fetch('http://localhost:3000/api');
-  const data: Review[] = await res.json();
+  const res = await fetch('http://localhost:3000/api', {
+    next: {
+      revalidate: 30
+    }
+  });
+  const data: productInfo[] = await res.json();
 
   return (
     <div>
       <ul>
-        {data.map((review: Review) => (
-            <li key={review.id} className='flex flex-col justify-center items-center'>
-              <h1 className='mt-10 font-bold'>{review.product_name}</h1>
-              <p>{review.vender_name}</p>
-              <h2>{review.star_rating}</h2>
+        {data.map((product: productInfo) => (
+            <li key={product.product_name} className='flex flex-col justify-center items-center'>
+              <h1 className='mt-10 font-bold'>{product.product_name}</h1>
+              <p>{product.vender_name}</p>
+              {/* <h2>{review.star_rating}</h2> // add later */}
               <Link 
                 className='mt-5 px-4 py-2 bg-[#005FF6] text-white rounded hover:bg-blue-700'
                 href='/?show=true'
