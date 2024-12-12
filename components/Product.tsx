1ae514375a48c5fe39b5a7cdbe51c2e6619f35bb
Grant Bentley <gbentley0618@comcast.net>
2024-12-12 16:05:40 -0600

    update line 20 to new api link

--- components/Product.tsx
+++ components/Product.tsx
@@ -17,7 +17,7 @@ type productInfo = {
 
 
 export default async function Product(){
-  const res = await fetch('http://localhost:3000/api', {
+  const res = await fetch('http://localhost:3000/api/Products', {
     next: {
       revalidate: 30
     }
@@ -31,7 +31,7 @@ export default async function Product(){
             <li key={product.product_name} className='flex flex-col justify-center items-center'>
               <h1 className='mt-10 font-bold'>{product.product_name}</h1>
               <p>{product.vender_name}</p>
-              {/* <h2>{review.star_rating}</h2> // add later */}
+              {/* add overall rating later */}
               <Link 
                 className='mt-5 px-4 py-2 bg-[#005FF6] text-white rounded hover:bg-blue-700'
                 href='/?show=true'
