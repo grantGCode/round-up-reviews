71000c5369551bf421a22cf60380595dcae82b3f
Grant Bentley <gbentley0618@comcast.net>
2025-01-09 16:21:11 -0600

    remove <p> Presents from Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,6 @@ function Header() {
       <div className='flex flex-col justify-center items-center' >  
       <div className='flex flex-col flex-wrap justify-center items-center'>
         <h4 className='text-white font-poppins'>Round Up Reviews</h4>
-        <p className='text-white font-poppins'>Presents</p>
       </div>
       </div>      
     </div>
