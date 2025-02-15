744a4092cb407614fc55e6e0246984bc173625ee
Grant Bentley <gbentley0618@comcast.net>
2025-02-14 23:33:11 -0600

    set up star raiting tab

--- components/modal/ProductModal.tsx
+++ components/modal/ProductModal.tsx
@@ -43,11 +43,11 @@ export default function ProductModal({
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-      <div className="p-8 border w-96 shadow-lg bg-[#E5E5E5]">
+      <div className="p-8 border w-96 shadow-lg bg-[#FFFFFF]">
           <Link href='/'>
             <Image src={ArrowLeft} alt='X'></Image>
           </Link>
-          <div className='flex flex-col justify-center'>
+          <div className='flex flex-col justify-center bg-[#FFFFFF]'>
             <h5 className='mt-10'>{productMetaData.vender_name}</h5>
             <h4 className='mt-10'>{productMetaData.product_name}</h4>
             <h4>Over All Star Rating</h4>
--- components/modal/SeeStarRatings.tsx
+++ components/modal/SeeStarRatings.tsx
@@ -1,14 +1,49 @@
 'use client'
 import Image from 'next/image'
-import PercentIcon from '../../public/Percent-Icon.png'
-import GrayVector from '../../public/Gray-Vector.png'
+// import PercentIcon from '../../public/Percent-Icon.png'
+// import GrayVector from '../../public/Gray-Vector.png'
+import StarYellow from '../../public/StarYellow.png';
+import StarGray from '../../public/StarGray.png';
+
 
 function SeeStarRatings() {
   return (
-    <div className='flex flex-row'>
-        <Image src={PercentIcon} alt='Percent Icon' />   
-        <h2>SeeStarRatings</h2>
-        <Image src={GrayVector} alt='Gray Vector' />
+    // Closed
+    // <div className='flex flex-row'>
+    //     <Image src={PercentIcon} alt='Percent Icon' />   
+    //     <h2>SeeStarRatings</h2>
+    //     <Image src={GrayVector} alt='Gray Vector' />
+    // </div>
+    // open
+    <div className='flex flex-col bg-[#F3F3F3]'>
+      <div className='flex flex-row'>
+        <div className='flex flex-row'>
+          <div className='flex items-center'>
+            <h2 className='flex font-bold'>
+              {/* <Image className='' src={StarYellow} alt={'Yellow Star'}></Image> */}
+              3/5 
+            </h2>
+            <div className='flex flex-row'>
+              <p className='text-[#000000]' >{`${5}`}</p>
+              <p className='text-[#BBB7C6]'>{`(${120} reviews)`}</p>
+            </div>
+          </div>
+        </div>
+          <div className='flex flex-col'>
+            <p>5 star</p>
+            <p>4 star</p>
+            <p>3 star</p>
+            <p>2 star</p>
+            <p>1 star</p>
+          </div>
+      </div>
+      <div id='Stars'className='flex flex-row justify-center items-center'>
+        <Image src={StarYellow} alt={'Yellow Star'}></Image>
+        <Image src={StarYellow} alt={'Yellow Star'}></Image>
+        <Image src={StarYellow} alt={'Yellow Star'}></Image>
+        <Image src={StarGray} alt={'Gray Star'}></Image>
+        <Image src={StarGray} alt={'Gray Star'}></Image>
+      </div>
     </div>
   )
 }
