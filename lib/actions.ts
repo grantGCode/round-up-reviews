835442dfd778305d093c5501164fffbfbe36b53d
Grant Bentley <gbentley0618@comcast.net>
2025-03-17 22:29:38 -0500

    reformat last commit

--- lib/actions.ts
+++ lib/actions.ts
@@ -43,5 +43,4 @@ export async function submitNewRating(formData: FormData): Promise<void> {
     } catch (error) {
         console.error("Failed to submit review:", error);
     }
-    revalidatePath('/')
   };
